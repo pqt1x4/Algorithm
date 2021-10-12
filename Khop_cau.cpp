867f80f3d74890c28f769ed40a565666aaf89c8d
Phan Quý Thịnh <90194701+pqt1x4@users.noreply.github.com>
2021-10-12 19:03:29 +0700

    Create Khop_cau.cpp

---
+++ Khop_cau.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int N = 100005;
+int n, m;
+vector<int> a[N];
+int CriticalEdge = 0;
+bool CriticalNode[N];
+int Num[N], Low[N], Time = 0;
+
+void visit(int u, int p) {
+    int NumChild = 0;
+    Low[u] = Num[u] = ++Time;
+    for (int v : a[u])
+        if (v != p) {
+            if (Num[v] != 0)
+                Low[u] = min(Low[u], Num[v]);
+            else {
+                visit(v, u);
+                NumChild++;
+                Low[u] = min(Low[u], Low[v]);
+
+                if (Low[v] >= Num[v])
+                    CriticalEdge++;
+
+                if (u == p) {
+                    if (NumChild >= 2)
+                        CriticalNode[u] = true;
+                } else {
+                    if (Low[v] >= Num[u])
+                        CriticalNode[u] = true;
+                }
+            }
+        }
+}
+
+int main() {
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= m; i++) {
+        int x, y;
+        scanf("%d%d", &x, &y);
+        a[x].push_back(y);
+        a[y].push_back(x);
+    }
+    for (int i = 1; i <= n; i++)
+        if (!Num[i]) visit(i, i);
+
+    int Count = 0;
+    for (int i = 1; i <= n; i++)
+        if (CriticalNode[i]) Count++;
+    printf("%d %d\n", Count, CriticalEdge);
+}
